c3e735ada280ed3dcbdfbdc3cc750b6b73b1cd5b
jrossdelacruz <2015081632@ust-ics.mygbiz.com>
2018-09-13 14:59:34 +0800

    Initial commit

---
+++ app/src/main/java/com/galon/bryan/galonbryanexam1/GalonBryanExam1.java
@@ -0,0 +1,13 @@
+package com.galon.bryan.galonbryanexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalonBryanExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galon_bryan_exam1);
+    }
+}
